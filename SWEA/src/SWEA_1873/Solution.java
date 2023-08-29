b6f13a58688638e21876ca0dba9c0ff01304f018
KuMMii <4.prgmng@gmail.com>
2023-08-29 21:20:50 +0900

    battlefield

--- SWEA/src/SWEA_1873/Solution.java
+++ SWEA/src/SWEA_1873/Solution.java
@@ -6,9 +6,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
-	static int R,C;
-	static char[][] field;
-	static char[] cmd;
+	static int R,C; //배열 길이
+	static char[][] field; //배틀필드 담는 배열
+	static char[] cmd; //커맨드 담는 배열
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		System.setIn(new FileInputStream("src/SWEA_1873/input.txt"));
@@ -31,20 +31,22 @@ public class Solution {
 			cmd=new char[N];
 			cmd=sc.next().toCharArray();
 			
+			//커맨드 순회
 			for(char ch : cmd ) {
 				if(ch=='S') shoot(); //포탄쏘는 메서드
 				else move(ch); //움직이는 메서드
-			}
+			}//cmd for
 			
 			
 			//출력
+			System.out.printf("#%d ",tc);
 			for(char[] ch : field) {
 				for(char i: ch) {
 					System.out.print(i);
 				}
 				System.out.println();
 			}
-			
+			System.out.println();
 		}//tc
 	}//main
 	
@@ -57,28 +59,38 @@ public class Solution {
 	static int[] dr={-1,1,0,0};
 	static int[] dc={0,0,-1,1};
 	
+	//방향
 	static int arrow;
 	
 	
 	//move
 	private static void move(char cmd) {
+		//현재 위치
 		findMe();
+		
+		//방향전환
+		switch(cmd) {
+		case 'U' : arrow=0; field[r][c]='^'; break;
+		case 'D' : arrow=1; field[r][c]='v'; break;
+		case 'L' : arrow=2; field[r][c]='<'; break;
+		case 'R' : arrow=3; field[r][c]='>'; break;
+		}//switch
 		
 		//원래 있던 자리 저장
 		int curR=r;
 		int curC=c;
-		
-		while(r+dr[arrow]>=0 || r+dr[arrow]<R || c+dc[arrow]>=0 || c+dc[arrow]<C 
-				|| field[r+dr[arrow]][c+dc[arrow]]!='.') {
-			
-			r+=dr[arrow];
-			c+=dc[arrow];
-			
-			System.out.println(222);
-		}//while
-		
+		char me=field[r][c]; //현재 화살표 저장
 		field[curR][curC]='.';
-		field[r][c]=cmd;
+			
+			if(r+dr[arrow]>=0 && r+dr[arrow]<R && c+dc[arrow]>=0 && c+dc[arrow]<C ) { //범위내에 있다면
+				if(field[r+dr[arrow]][c+dc[arrow]]=='.') { //평지면
+					//움직임
+					r+=dr[arrow]; 
+					c+=dc[arrow];
+				}
+			}
+		//움직인 자리에 그 화살표 저장해줌
+		field[r][c]=me;
 		
 	}//move
 	
@@ -90,19 +102,26 @@ public class Solution {
 		findMe();//탱크 위치
 		int tr=r;
 		int tc=c;
+		
 		//벽을 만나면 멈추고, 범위를 벗어나면 멈춤
-		while(tr+dr[arrow]>=0 || tr+dr[arrow]<R || tc+dc[arrow]>=0 || tc+dc[arrow]<C //범위 벗어남
-				|| field[tr+dr[arrow]][tc+dc[arrow]]!='#' || field[tr+dr[arrow]][tc+dc[arrow]]!='*') {
+		while(true) {
 			
+			if((tr+dr[arrow])<0 || (tr+dr[arrow])>=R || (tc+dc[arrow])<0 || (tc+dc[arrow])>=C){ //범위 넘어가면
+				break;
+			}
+			if(field[(tr+dr[arrow])][(tc+dc[arrow])]=='#') { //벽 만나면
+				break;
+			}
+			if(field[(tr+dr[arrow])][(tc+dc[arrow])]=='*') { //벽돌벽 만나면
+				field[(tr+dr[arrow])][(tc+dc[arrow])]='.';
+				break;
+			}
 			//그 방향으로 한칸 더 가기
 			tr+=dr[arrow];
 			tc+=dc[arrow];
-			System.out.println(111);
 			
 		}//while
 		
-		//다 돌고 벽돌을 맞은 거면 평지로 바꿈
-		if(field[tr+dr[arrow]][tc+dc[arrow]]=='*') field[tr+dr[arrow]][tc+dc[arrow]]='.';
 		
 	}//shoot
 
--- SWEA/src/SWEA_4613/Solution.java
+++ SWEA/src/SWEA_4613/Solution.java
@@ -7,28 +7,25 @@ import java.util.Scanner;
 
 public class Solution {
 	
-	static char[][] arr, tmp;
-	static int before,sum,min,R,C,fix=0;
-	static int[][] save;
+	static char[][] arr;
+	static int sum,min,R,C;
 	
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		System.setIn(new FileInputStream("src/SWEA_4613/input.txt"));
+		System.setIn(new FileInputStream("src/SWEA_4613/input2.txt"));
 		Scanner sc=new Scanner(System.in);
 		
 		int T=sc.nextInt();
 		
-		for(int tc=1; tc<=1; tc++) {
-			
-			fix=0;
-			sum=0;
-			min=2500;
+		for(int tc=1; tc<=T; tc++) {
 			
 			R=sc.nextInt();
 			C=sc.nextInt();
 			
 			arr=new char[R][C];
+			
+			min=2500;
 			
 			//깃발 입력
 			for(int i=0; i<R; i++) {
@@ -36,120 +33,41 @@ public class Solution {
 				arr[i]=tmpStr.toCharArray();
 			}
 			
-			char[][] tmp=new char[R][C];
-			for(int i=0; i<R; i++) {
-				for(int j=0; j<C; j++) {
-					tmp[i][j]=arr[i][j];
-				}
-			}
 			
-			for(int i=0;i<C; i++) {
-				if(tmp[0][i]!='W') {
-					fix++;
-					tmp[0][i]='W';
-				}
-				if(tmp[R-1][i]!='R') {
-					fix++;
-					tmp[R-1][i]='R';
-				}
-				
-			}//for 맨위아래 바꾸기
+			for(int l=1; l<=R-2; l++) { //길이용 l
+				//가로 움직이기
+				for(int r=1; r<=R-1-l; r++) { //row idx
+					sum=0;
+					
+					for(int c=0; c<C; c++) { //col idx
+						
+						
+						//blue
+						for(int i=0; i<l;i++) {//두께
+							if(arr[r+i][c]!='B') sum++;
+						}//for i
+					
+						//white
+						for(int j=0; j<r;j++) {
+							if(arr[j][c]!='W') sum++;
+						}//for white
+						
+						//red
+						for(int j=r+l; j<R;j++) {
+							if(arr[j][c]!='R') sum++;
+						}//for red
+						
+						
+					
+					}//for c 
+					min=Math.min(min, sum);
+				}//for r
+			}//for l
 			
-			//중간부분 B로 바꾸기
-			for(int i=1; i<R-1; i++) {
-				for(int j=0; j<C; j++) {
-					if(tmp[i][j]!='B') {
-						sum++;
-					}
-				}
-			}//for i
-			System.out.println("f" +fix);
-			save=new int[R-2][R-2];
-			save[0][R-3]=sum;
-			before=sum;
-			middle(0,R-3);
-			
-			System.out.println("min:"+min);
-			System.out.printf("#%d %d\n",tc,min+fix);
+			System.out.printf("#%d %d\n",tc,min);
 		}//tc
 		
 	}//main
-	
-	
-
-	private static void middle(int upIdx, int downIdx) {
-		
-		
-		System.out.println("sum위 : "+sum);
-
-		//전값을 설정
-		if(upIdx-1<0 && downIdx+1>=save.length) {
-			before=save[upIdx][downIdx];
-			System.out.println(11111);
-		}else if(upIdx-1<0) {
-			before=save[upIdx][downIdx+1];
-			System.out.println(2222);
-		}else if(downIdx+1>=save.length) {
-			before=save[upIdx+1][downIdx];
-			System.out.println(3333);
-		}
-		
-		System.out.println("before : "+before);
-		char[][] tmp=new char[R-2][C];
-		System.out.println("up : "+upIdx+" down : "+downIdx );
-		
-		//맨위, 맨 아래 제외한 부분 복사한 배열 생성
-		for(int i=0; i<R-2; i++) {
-			for(int j=0; j<C; j++) {
-				tmp[i][j]=arr[i+1][j];
-			}
-		}//for
-
-		if(downIdx-upIdx<0) return;
-		
-		int cnt=0;
-		sum=0;
-
-		
-		//upIdx 윗줄을 무조건 B빼고 W로 바꾸기
-		for(int j=0; j<C; j++) {//B바꾼거 더한거 빼기
-			if(upIdx-1>=0) {
-				if(tmp[upIdx-1][j]!='B') { //B
-					cnt++;
-				}
-				if(tmp[upIdx-1][j]!='W') { //W
-					sum++;
-				}
-				
-			}//윗 줄이 범위내에 있다면
-			
-			//down보다 한줄 밑
-			if(downIdx+1<tmp.length) {
-				if(tmp[downIdx+1][j]!='B') { //B
-					cnt++;
-				}
-				if(tmp[downIdx+1][j]!='R') { //W
-					sum++;
-				}
-				
-			}//범위내에 아랫줄이 있는지 확인
-		}//for B
-		
-		System.out.println("cnt최종 : "+cnt);
-		
-		System.out.println("sum 합 : "+sum);
-		
-		sum+=(before-cnt);
-		save[upIdx][downIdx]=sum;
-		
-		System.out.println("sum보내는값 : "+sum);
-		min=Math.min(min, sum);
-		
-		if(upIdx+1<tmp.length) middle(upIdx+1,downIdx);
-		if(upIdx-1>=0) middle(upIdx,downIdx-1);
-		if(upIdx+1<tmp.length && upIdx-1>=0) middle(upIdx+1,downIdx-1);
-		
-	}//middle
 }//class
 
 
