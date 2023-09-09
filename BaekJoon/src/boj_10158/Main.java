ae9b2c8d895757e9fc8a38928fac64c5e4f9a246
KuMMii <4.prgmng@gmail.com>
2023-09-09 13:46:48 +0900

    let me see

--- BaekJoon/src/boj_10158/Main.java
+++ BaekJoon/src/boj_10158/Main.java
@@ -3,21 +3,18 @@ package boj_10158;
 import java.util.Scanner;
 
 public class Main {
-	static int arr[][];
-	static int R,C,r,c,T;
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		C=sc.nextInt();
-		R=sc.nextInt();
+		int C=sc.nextInt();
+		int R=sc.nextInt();
 		
 		//first position
-		c=sc.nextInt();
-		r=sc.nextInt();
+		int c=sc.nextInt();
+		int r=sc.nextInt();
 		
 		//time
-		T=sc.nextInt();
+		int T=sc.nextInt();
 		
-		arr=new int[R+1][C+1];
 		
 		//둘 다 +,+
 		int moveR=1;
