82f2b8ac7f1eba0e11339acdf8c8d5b162518a14
KuMMii <4.prgmng@gmail.com>
2023-10-12 09:38:54 +0900

    20056 boj

--- BaekJoon/src/boj_20056/Main.java
+++ BaekJoon/src/boj_20056/Main.java
@@ -39,7 +39,7 @@ public class Main {
 		M=Integer.parseInt(st.nextToken());
 		K=Integer.parseInt(st.nextToken());
 		
-		arr=new int[N+1][N+1][4];
+		arr=new int[N+1][N+1][5];//질량m,속력s, 방향d의 홀수짝수, 방향d
 		q=new LinkedList<>();
 		
 		for(int i=0; i<M; i++) {
@@ -56,21 +56,26 @@ public class Main {
 		////input fin
 		
 		//K번 돌리기
-		for(int k=0; k<=K; k++) {
+		for(int k=0; k<K; k++) {
 			//큐에 있는만큼만 돌리기
 			int T=q.size();
 			//arr에 정보 다 더해서 저장
 			for(int t=0; t<T; t++) {
 				ball b=q.poll();
+				
+				//////공 출력
+//				System.out.println(k+1+"회");
+//				System.out.printf("좌표 : (%d, %d), 질량 : %d, 속력 : %d, 방향 : %d\n", b.R,b.C,b.M, b.S,b.D);
+				
 //				System.out.printf("%d %d %d\n",b.M,b.S,b.D);
 				//공의 새로운 위치
 				int ballR=Math.abs(b.R+dr[b.D]*b.S)%N;
 				if(ballR==0) ballR=N;
-				else if(ballR<0) ballR=ballR+N;
+				else if(ballR<0) ballR=ballR+N+1;
 				
 				int ballC=Math.abs(b.C+dc[b.D]*b.S)%N;
 				if(ballC==0) ballC=N;
-				else if(ballC<0) ballC=ballC+N;
+				else if(ballC<0) ballC=ballC+N+1;
 				
 //				System.out.printf("R : %d, C : %d\n", ballR,ballC);
 				
@@ -78,6 +83,7 @@ public class Main {
 				arr[ballR][ballC][1]+=b.S;
 				arr[ballR][ballC][2]++;
 				arr[ballR][ballC][3]+=(b.D%2);
+				arr[ballR][ballC][4]+=b.D;
 			}//arr에 덧셈끝
 			
 			for(int i=1; i<=N; i++) {
@@ -88,11 +94,8 @@ public class Main {
 					int quan=arr[i][j][2];
 					//1개인 경우는 큐에 바로 추가
 					if(quan==1) {
-						q.add(new ball(i,j,arr[i][j][0],arr[i][j][1],arr[i][j][3]));
-						//0으로 초기화
-						for(int z=0; z<4; z++) {
-							arr[i][j][z]=0;
-						}
+						q.add(new ball(i,j,arr[i][j][0],arr[i][j][1],arr[i][j][4]));
+						
 						
 					}
 					//1개 이상이면 수술...들어가야함
@@ -115,17 +118,39 @@ public class Main {
 							}
 						}
 						
-						//0으로 초기화
-						for(int z=0; z<4; z++) {
-							arr[i][j][z]=0;
-						}
 						
 					}
 					
 					
+				}
+			}
+			/////////////////////////
+			//출력
+//			for(int I=1; I<=N; I++) {
+//				for(int J=1; J<=N; J++) {
+//					System.out.print(arr[I][J][0]+" ");
+//							System.out.print(arr[I][J][2]+" ");
+//					
+//				}
+//				System.out.println();
+//			}
+			
+			/////////////////////////////////
+//			System.out.println();
+			//0으로 초기화
+			for(int I=1; I<=N; I++) {
+				for(int J=1; J<=N; J++) {
+					for(int z=0; z<4; z++) {
+						arr[I][J][z]=0;
+					}
 					
 				}
 			}
+			
+//			for(ball Ball : q) {
+//				System.out.println(Ball.M);
+//			}
+			
 			
 		}//K번 돌리기
 		
