d0f7e09b020d7e0b60dce0f2e3bc20134149d5b9
KuMMii <4.prgmng@gmail.com>
2023-10-31 18:05:33 +0900

    116786

---
+++ BaekJoon/src/boj_11657/Main.java
@@ -0,0 +1,26 @@
+package boj_11657;
+
+import java.util.List;
+
+public class Main {
+	static class node{
+		int s;
+		int e;
+		int t;
+		public node(int s, int e, int t) {
+			super();
+			this.s = s;
+			this.e = e;
+			this.t = t;
+		}
+		
+	}//node
+	
+	static int N;
+	static int[] time;
+	static List<node> edges;
+	
+	public static void main(String[] args) {
+		
+	}//main
+}//class
