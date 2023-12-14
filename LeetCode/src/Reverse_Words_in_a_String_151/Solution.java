336a793debc7406e6c1ac8bbbfc24187f853ad3c
KuMMii <4.prgmng@gmail.com>
2023-12-15 00:44:24 +0900

    231244 leetcode 151

---
+++ LeetCode/src/Reverse_Words_in_a_String_151/Solution.java
@@ -0,0 +1,51 @@
+package Reverse_Words_in_a_String_151;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "a good   example";
+        reverseWords(s);
+    }
+
+    public static String reverseWords(String s) {
+        //pointer 만들기
+        int pointer = 0;
+        List<String> list = new ArrayList<>();
+
+        //loop until the end of String
+        while (pointer < s.length()) {
+            StringBuilder word = new StringBuilder();
+
+            //if char is not a blank, append to the word
+            if (s.charAt(pointer) != ' ') {
+                while (s.charAt(pointer) != ' ') {
+                    word.append(s.charAt(pointer));
+                    if (pointer == s.length() - 1) {
+                        break;
+
+                    }
+                    pointer++;
+                }
+                list.add(word.toString());
+            }
+
+            pointer++;
+        }
+
+        Collections.reverse(list);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i));
+            if (i != list.size() - 1) {
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+}
---
+++ LeetCode/src/Reverse_Words_in_a_String_151/Solution2.java
@@ -0,0 +1,41 @@
+package Reverse_Words_in_a_String_151;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        String s = "  hello world  ";
+        reverseWords(s);
+    }
+
+    public static String reverseWords(String s) {
+
+        int n = s.length();
+
+        //pointer 만들기
+        int p1 = n - 1;
+
+        //char[]
+        char[] chars = s.toCharArray();
+
+        //sb to return
+        StringBuilder sb = new StringBuilder();
+
+        //loop to the first of the s
+        while (p1 >= 0) {
+            if (chars[p1] == ' ') {
+                p1--;
+                continue;
+            }
+            //if it is not a space
+            int p2 = p1 - 1;
+
+            while (p2 >= 0 && chars[p2] != ' ') {
+                p2--;
+            }
+            sb.append(s.substring(p2 + 1, p1 + 1));
+            sb.append(' ');
+            p1 = p2 - 1;
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+}
